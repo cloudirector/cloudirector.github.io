4b3f4e1c45763eb9461ed478c7e03ec9abc7eff7
cloudirector <143876484+cloudirector@users.noreply.github.com>
2024-02-23 09:25:41 -0600

    Update and rename capture-screenshot.j to capture-screenshot.js

--- capture-screenshot.js
+++ capture-screenshot.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://your-website-url.com');  // Replace with your actual website URL
+  await page.goto('https://cloudirector.github.io');
   await page.screenshot({ path: 'screenshot.png' });
   await browser.close();
 })();
