2f62dce7f3b782ca7d92daae5852a3944d1bc1b2
cloudirector <143876484+cloudirector@users.noreply.github.com>
2024-11-05 20:24:06 +0000

    Add games button and subdirectory

--- js/script.js
+++ js/script.js
@@ -1,34 +1,40 @@
-const githubBtn = document.getElementById('github-button')
-const themeToggleBtn = document.getElementById('theme-toggle');
-const lightIcon = document.getElementById('theme-toggle-light-icon');
-const darkIcon = document.getElementById('theme-toggle-dark-icon');
+const gamesBtn = document.getElementById("games-button");
+const githubBtn = document.getElementById("github-button");
+const themeToggleBtn = document.getElementById("theme-toggle");
+const lightIcon = document.getElementById("theme-toggle-light-icon");
+const darkIcon = document.getElementById("theme-toggle-dark-icon");
 
 function setTheme(theme) {
-    document.documentElement.setAttribute('data-theme', theme);
-    if (theme === 'dark') {
-        lightIcon.classList.add('hidden');
-        darkIcon.classList.remove('hidden');
+    document.documentElement.setAttribute("data-theme", theme);
+    if (theme === "dark") {
+        lightIcon.classList.add("hidden");
+        darkIcon.classList.remove("hidden");
     } else {
-        lightIcon.classList.remove('hidden');
-        darkIcon.classList.add('hidden');
+        lightIcon.classList.remove("hidden");
+        darkIcon.classList.add("hidden");
     }
-    localStorage.setItem('theme', theme);
+    localStorage.setItem("theme", theme);
 }
 
-const savedTheme = localStorage.getItem('theme');
+const savedTheme = localStorage.getItem("theme");
 if (savedTheme) {
     setTheme(savedTheme);
 } else {
-    setTheme('dark');
+    setTheme("dark");
 }
 
-githubBtn.addEventListener('click', () => {
-    const url = 'https://github.com/cloudirector/';
-    window.open(url, '_blank').focus();
+gamesBtn.addEventListener("click", () => {
+    const url = "/games/";
+    window.open(url, "_blank").focus();
 });
 
-themeToggleBtn.addEventListener('click', () => {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+githubBtn.addEventListener("click", () => {
+    const url = "https://github.com/cloudirector/";
+    window.open(url, "_blank").focus();
+});
+
+themeToggleBtn.addEventListener("click", () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
     setTheme(newTheme);
 });
